501d5af74b9f26531a65bb3c9a82ab6cb3348919
arivazhaki <marivazhaki@gmail.com>
2024-05-22 16:04:39 +0530

    Completion of Cake

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import './App.scss';
 
 const App = () =>
 	<div className="App">
-		Ready to start.
-	</div>;
+		<div className="oval"/>
+		<div className="wick"/>
+		<div className="candle"/>
+		<div className="boxParentChild5"/>
+		<div className="boxParentChild4"/>
+		<div className="boxParentChild3"/>
+		<div className="strawberryLeft"/>
+		<div className="strawberryRight"/>
+		<div className="seed"/></div>;
 
 export default App;
