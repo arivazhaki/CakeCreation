2238b5ce93b7406dab059adb869e7256dab37d25
arivazhaki <marivazhaki@gmail.com>
2024-05-22 13:26:42 +0530

    Initialized.

---
+++ src/components/Cake.js
@@ -0,0 +1,12 @@
+import { React } from 'react';
+import './App.scss';
+
+const Cake = () =>
+	<div className="App">
+		<div className="oval"/>
+		<div className="wick"/>
+		<div className="candle"/>
+		<div className="boxParentChild5"/>
+	</div>;
+
+export default Cake;
